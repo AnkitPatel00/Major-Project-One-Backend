b65b87ea5461e959ff312b03bcdc971cd01b0b0d
Ankit Patel <165417853+AnkitPatel00@users.noreply.github.com>
2024-12-04 10:58:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/', (req,res) => {
     res.send("Hello Clothing Server")  
 })
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Api Route not found' });
+});
+
 // ************ JWT Middleware ***********
 
 // jwt auth
