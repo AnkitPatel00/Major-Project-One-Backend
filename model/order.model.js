7c2de7dfff48120d142554681636d98bbfd88807
Ankit Patel <165417853+AnkitPatel00@users.noreply.github.com>
2024-12-06 14:38:13 +0530

    Update order.model.js

--- model/order.model.js
+++ model/order.model.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
 
+const orderSchema = new mongoose.Schema({
+  user: { type: ObjectId, ref: "clothusers" },
+  address: { type: ObjectId, ref: "clothusersaddress" },
+  cloths: [{ clothId: { type: ObjectId, ref: "ClothStore" }, quantity: { type: Number }, size: { type: String } }],
+  date: { type: Date, default: Date.now },
+  totalPrice: {type: Number}
+},{timestamps:true})
+
+
+
+const OrderModel = mongoose.model('clothsorders', orderSchema)
+
+module.exports = OrderModel
