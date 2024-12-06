a66b957fb6c0c075053becc23e099633bbb88cb5
Ankit Patel <165417853+AnkitPatel00@users.noreply.github.com>
2024-12-06 14:53:00 +0530

    Update order.model.js

--- model/order.model.js
+++ model/order.model.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
   user: { type: ObjectId, ref: "clothusers" },
   address: { type: ObjectId, ref: "clothusersaddress" },
   cloths: [{ clothId: { type: ObjectId, ref: "ClothStore" }, quantity: { type: Number }, size: { type: String } }],
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: new Date().toDateString() },
   totalPrice: {type: Number}
 },{timestamps:true})
 
